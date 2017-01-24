7f0b9cc5ba4f35a49fd8f5232bee81e85dd21b41
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2017-01-24 01:01:40 -0500

    Flight Sim

---
+++ Flight Simulator/Assets/Scripts/Fire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    public GameObject Ammo;
+    public Transform AmmoSpawnPoint;
+
+    [Range(0, 1)]
+    public float FireRate;
+    [Range(2, 250)]
+    public float FireForce;
+
+
+    private float nextShot = 0.0F;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextShot)
+        {
+            //Debug.Log("Time: " + Time.time + ", nextDig: " + nextDig);
+            nextShot = Time.time + FireRate;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        var bullet = GameObject.Instantiate(Ammo, AmmoSpawnPoint.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * FireForce, ForceMode.Impulse);
+    }
+}
---
+++ Flight Simulator/Assets/Scripts/JetController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetController : MonoBehaviour {
+
+    [Range(50,1000)]
+    public float MaxVelocity = 100;
+    [Range(1, 100)]
+    public float Accelleration = 5;
+    [Range(1, 100)]
+    public float ViewRange = 50;
+    [Range(1, 100)]
+    public float RotateSpeed = 10; 
+
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) && rb.velocity.magnitude < MaxVelocity)
+        {
+            rb.AddForce(transform.forward * Accelleration);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && rb.velocity.magnitude > 0)
+        {
+            rb.AddForce(-transform.forward * Accelleration/2);
+        }
+        var viewPlane = new Plane(-Camera.main.transform.forward, Camera.main.transform.position + Camera.main.transform.forward * ViewRange);
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var hitdist = 0.0f;
+
+        if (viewPlane.Raycast(ray,out hitdist))
+        {
+            var targetPoint = ray.GetPoint(hitdist);
+
+            var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotateSpeed * Time.fixedDeltaTime);
+        }
+
+    }
+}
